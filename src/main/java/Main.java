ef170a77b9cc6e9e512af26cf6ed31f70607c352
Burak Köken <burakkokenn@gmail.com>
2017-07-07 01:27:08 +0200

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+		Person person1 = context.getBean("person1", Person.class);
+		
+		System.out.println(person1);
+	}
+
+}
